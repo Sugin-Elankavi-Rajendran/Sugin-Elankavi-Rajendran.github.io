6dfb92140673f2b12ccd13cb9844f29a76f7d33a
Sugin Elankavi Rajendran <57343300+sugin22@users.noreply.github.com>
2024-02-23 17:57:28 +0530

    website update

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Sugin Elankavi Rajendran";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Robotics", "Mechatronics", "Pipeline Inspection", "Condition Monitoring", "Research"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
